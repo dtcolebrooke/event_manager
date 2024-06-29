a340e7e06808410b9503ac967e698d15cfce2f64
Devin Tobias Colebrooke <dt.colebrooke@playraffleparty.com>
2024-06-28 22:46:42 -0700

    Refactored our Ruby Parser

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,1 +1,28 @@
-puts 'Event Manager Initialized!'
+class EventManager
+  require 'csv'
+
+  attr_accessor :lines
+
+   def initialize
+    @lines = File.readlines 'F:/repos/event_manager/event_attendees.csv'
+    start
+   end
+# Does this File actually exist? File check with method.
+# puts File.exist? 'F:/repos/event_manager/event_attendees.csv'
+
+def start
+  puts 'Event Manager Initialized!'
+end
+
+def first_names(names: @lines)
+  names.each_with_index do |line, index|
+    next if index == 0  
+    columns = line.split(',')
+    name = columns[2]
+    puts name
+  end
+ end
+
+ attendees = EventManager.new
+ attendees.first_names
+end
