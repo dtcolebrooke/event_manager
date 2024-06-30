8385bf65d65cac85b62c6afa935ec2e125060584
Devin Tobias Colebrooke <dt.colebrooke@playraffleparty.com>
2024-06-29 19:31:40 -0700

    Corrected formatting using Rubocop -a & -A. As well as, further DRY'd the code out

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
+# gets first names of attendees
 class EventManager
   require 'csv'
 
-  attr_accessor :lines
+  attr_accessor :lines, :first_names
 
   def initialize
     @lines = CSV.open('F:/repos/event_manager/event_attendees.csv', headers: true)
@@ -9,20 +12,24 @@ class EventManager
   end
 
   def start
-  puts 'Event Manager Initialized!'
+    puts 'Event Manager Initialized!'
   end
 
   def list_first_names(file: @lines)
     file.each do |row|
-      print_the_name(row)
+      retrieve_the_name(row)
     end
-   end
-
-  def print_the_name(row)
-      first_names = row[2]
-      puts first_names
   end
 
-   attendees = EventManager.new
-   attendees.list_first_names
+  def retrieve_the_name(row)
+    @first_names = row[2]
+    list_name(names: @first_names)
+  end
+
+  def list_name(names:)
+    puts names
+  end
+
+  attendees = EventManager.new
+  attendees.list_first_names
 end
