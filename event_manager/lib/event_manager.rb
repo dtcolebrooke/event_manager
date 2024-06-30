9d9c1c0e6fe906cf9026b10f8544e91325214afa
Devin Tobias Colebrooke <dt.colebrooke@playraffleparty.com>
2024-06-29 18:10:19 -0700

    Accessing the column values by the headers

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -3,24 +3,26 @@ class EventManager
 
   attr_accessor :lines
 
-   def initialize
+  def initialize
     @lines = CSV.open('F:/repos/event_manager/event_attendees.csv', headers: true)
     start
-   end
-# Does this File actually exist? File check with method.
-# puts File.exist? 'F:/repos/event_manager/event_attendees.csv'
-
-def start
-  puts 'Event Manager Initialized!'
-end
-
-def first_names(names: @lines)
-  names.each do |row|
-    name = row[2]
-    puts name
   end
- end
 
- attendees = EventManager.new
- attendees.first_names
+  def start
+  puts 'Event Manager Initialized!'
+  end
+
+  def list_first_names(file: @lines)
+    file.each do |row|
+      print_the_name(row)
+    end
+   end
+
+  def print_the_name(row)
+      first_names = row[2]
+      puts first_names
+  end
+
+   attendees = EventManager.new
+   attendees.list_first_names
 end
